cb8f5ee7ef93ab158b9a04590812abd9c26d6627
okanharenan <okanharenan@gmail.com>
2026-03-30 21:43:03 -0300

    Adjustment in console.log

--- atividade2.js
+++ atividade2.js
@@ -15,7 +15,12 @@ let sub = Number(number1) - Number(number2)
 let mult = Number(number1) * 50
 let div = 600 / Number(number2)
 
-console.log(`A soma do número 1 com o número 2 + 10 é: ${sum}`)
-console.log(`A subtração do número 1 com o número 2 é: ${sub}`)
+console.log(`A soma do número 1 com o número 2 + 10 é: ${sum} \n 
+    A subtração do número 1 com o número 2 é: ${sub} \n 
+    A multiplicação do número 1 por 50 é: ${mult} \n
+     A divisão de 600 pelo número 2 é: ${div}`)
+
+
+/*console.log(`A subtração do número 1 com o número 2 é: ${sub}`)
 console.log(`A multiplicação do número 1 por 50 é: ${mult}`)
-console.log(`A divisão de 600 pelo número 2 é: ${div}`)
+console.log(`A divisão de 600 pelo número 2 é: ${div}`)*/
