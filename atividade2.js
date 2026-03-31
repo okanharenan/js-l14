99456bfa059b76f85bec36ee1262ef14e2b27222
okanharenan <okanharenan@gmail.com>
2026-03-30 21:53:40 -0300

    adjustment in js

--- atividade2.js
+++ atividade2.js
@@ -1,11 +1,3 @@
-//leia 2 números do usuário. Faça a conversão com a função mais adequada e realize as operações abaixo:
-// some o número 1 com o número 2 e 10
-// subtraia o numero 2 do numero 1
-// multiplique o número 1 por 50
-// divida 600 pelo número 2
-
-//mostre no terminal todas as operações realizadas no seguinte formato: 
-// "A soma do número 1 com o número 2 e 10 é: resultado"
 
 let number1 = prompt("Digite o primeiro número: ")
 let number2 = prompt("Digite o segundo número: ")
@@ -19,8 +11,3 @@ console.log(`A soma do número 1 com o número 2 + 10 é: ${sum} \n
     A subtração do número 1 com o número 2 é: ${sub} \n 
     A multiplicação do número 1 por 50 é: ${mult} \n
      A divisão de 600 pelo número 2 é: ${div}`)
-
-
-/*console.log(`A subtração do número 1 com o número 2 é: ${sub}`)
-console.log(`A multiplicação do número 1 por 50 é: ${mult}`)
-console.log(`A divisão de 600 pelo número 2 é: ${div}`)*/
