ecb02f777aeb08257536b58b5d99da2d93f23be7
okanharenan <okanharenan@gmail.com>
2026-04-15 20:58:22 -0300

    Refactor patient queue management logic to alternate between priority and normal patients

--- atividades/Aula12/exercicio3.js
+++ atividades/Aula12/exercicio3.js
@@ -3,19 +3,15 @@ let filaNormal = [ 'Ana', 'João', 'Maria', 'Pedro', 'Joana', 'Paulo' ]
 let filaPrioritaria = ['Antonio', 'Isaias', 'Adalgisa', 'Adalberto']
 let atendidos = []
 
-//Para o atendimento serão liberadas 4 senhas. A ordem de atendimento será sempre 1 prioritária, 1 normal até que as senhas sejam esgotadas.
-//Cada paciente atendido ( que saiu da fila de atendimento) deverá ser removido da fila e adicionado ao final da fila de Atendidos. 
-
-//métodos para remoção: shift (inicio) e pop (final), sempre retornam o item removido
-//métodos para adição: push(final) e unshift(inicio)
-
-for (let i = 0; i < 4; i++){
-    let pacientePrioritario = filaPrioritaria.shift()
-    let pacienteNormal = filaNormal.shift()
-    if(pacientePrioritario){
+for (let i = 1; i <= 4; i++){
+    let pacientePrioritario
+    let pacienteNormal 
+    if( i % 2 ===1){
+        pacientePrioritario = filaPrioritaria.shift()
         atendidos.push(pacientePrioritario)
     }
-    if(pacienteNormal){
+    if(i % 2 === 0){
+        pacienteNormal = filaNormal.shift()
         atendidos.push(pacienteNormal)
     }
     
