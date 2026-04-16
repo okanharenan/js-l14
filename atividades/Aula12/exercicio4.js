d175735e47726ee38870ce804fa5bf67ee50ceb7
okanharenan <okanharenan@gmail.com>
2026-04-15 21:14:36 -0300

    Add JavaScript exercise for calculating student averages and update HTML reference

---
+++ atividades/Aula12/exercicio4.js
@@ -0,0 +1,18 @@
+//faça o calculo da média e mostre no console, para cada aluno
+let alunosNotas = [
+    ['joão', 10,10,8],
+    ['maria', 9,10,9],
+    ['pedro', 8,7,6],
+    ['ana', 7, 5, 7],
+    ['joana', 6, 8, 10]
+]
+//soma3Notas/3
+for (let i = 0; i < alunosNotas.length; i++){
+    let aluno = alunosNotas[i][0]
+    soma = 0
+    for(let j=1; j < alunosNotas[i].length; I++){
+        soma += alunosNotas[i][j]
+    }
+    let media = soma / (alunosNotas[i].length - 1)
+    console.log(`Média aluno(a): ${aluno} - Média: ${media.toFixed(2)}`)
+}
