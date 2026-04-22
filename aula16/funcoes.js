b334e0a23d7d62fd142d12a7c59ccd84b2040f2e
okanharenan <okanharenan@gmail.com>
2026-04-22 19:11:40 -0300

    Add basic mathematical functions and HTML structure

---
+++ aula16/funcoes.js
@@ -0,0 +1,28 @@
+//crie 4 funções para as operações matemáticas básicas: soma, subtração, multiplicação e divisão. Cada função deve receber dois parâmetros e mostrar o resultado da operação no console. Importante, não existe divisão por zero, então a função de divisão deve verificar se o segundo parâmetro é diferente de zero antes de realizar a operação. Caso contrário, deve mostrar uma mensagem de erro no console.
+
+//após criar as funções teste cada uma delas, passando valores diferentes para os parâmetros, incluindo casos de divisão por zero para verificar se a mensagem dexibie erro é da corretamente.
+
+function soma(a, b){
+    console.log(`A soma de a + b é: ${a + b}`)
+}
+
+function subtracao(a, b){
+    console.log(`A subtração de a - b é: ${a - b}`)
+}
+
+function multiplicacao(a, b){
+    console.log(`A multiplicação de a x b é: ${a * b}`)
+}
+
+function divisao(a, b){
+    if(b === 0){
+        console.log("Erro: divisão por zero não é permitida")
+    } else {
+        console.log(`A divisão de a + b é: ${a % b}`)
+    }
+}
+
+console.log(soma(10, 20))
+console.log(subtração(10,3))
+console.log(multi(30,2))
+console.log(divisao(10,2))
