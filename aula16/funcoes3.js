60cbb39b16d0ad5f986450d4b88eae7f48aa2e21
okanharenan <okanharenan@gmail.com>
2026-04-22 20:45:45 -0300

    Add funcoes3.js with functions for salary adjustment and attribute verification

---
+++ aula16/funcoes3.js
@@ -0,0 +1,48 @@
+//Você precisa criar 3 funções: uma função será responsável por calcular um adicional, recebendo uma porcentagem e um valor. Essa função retornará o valor com adicional.
+//A segunda função deverá verficar a existencia de um atributo de um objeto, recebendo o nome do atributo e o objeto. A função deve retornar true ou false.
+//A terceira função deverá receber um objeto e retornar uma cópia desse objeto modificado, obedecendo as seguntes regras:
+// o salário será aumentado em 15% se o funcionário tiver mais de 5 anos de empresa. 
+// o atributo dependentes deve ser adicionado ao objeto, caso o funcionário tenha filhos. O valor do atributo dependentes deve ser igual ao número de filhos do funcionário.
+//DICA: quando o objeto não tiver o atributo filhos, você deverá considerar que o funcionário não tem filhos.
+
+//A terceira função deve utilizar as duas primeiras funções para realizar as modificações necessárias no objeto.
+
+function calcularDesconto(valor, desconto){
+    return valor + (valor * desconto / 100)
+}
+
+function temAtributo( objt, atributo){
+    return Object.keys(objt).includes(atributo)
+
+}
+
+function atualizaFuncionario(funcionario){
+    let novoFuncionario = [... funcionario]
+
+    if(novoFuncionario.anosEmpresa>5){
+        novoFuncionario.salario = calcularDesconto(novoFuncionario.salario, 15)
+    }
+    if (temAtributo(novoFuncionario, "filhos") && novoFuncionario.filhos > 0){
+        novoFuncionario.dependentes  = novoFuncionario.filhos
+    }
+    return novoFuncionario
+}
+
+
+let func = [
+    {
+        nome: "João",
+        salario: 2000,
+        anosEmpresa: 6,
+        filhos: 2
+    },
+    {
+        nome: "Maria",
+        salario: 3000,
+        anosEmpresa: 3
+    }
+]
+
+
+console.log(func[0].nome) // joao
+console.log(func[1].nome) // maria
