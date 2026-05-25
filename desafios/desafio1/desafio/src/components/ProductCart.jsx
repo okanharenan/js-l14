22f32dd155cd849e2648576becafa83b1f54d569
okanharenan <okanharenan@gmail.com>
2026-05-25 20:19:31 -0300

    feat: enhance ProductCart styling and add image link functionality

--- desafios/desafio1/desafio/src/components/ProductCart.jsx
+++ desafios/desafio1/desafio/src/components/ProductCart.jsx
@@ -1,7 +1,7 @@
 export default function ProductCart({product, addToCart}){
     return(
         <div className="product-cart">
-            <img src={product.image} alt={product.name} />
+            <a href={product.image} target="_blank"><img src={product.image} alt={product.name} /></a>
             <h2>{product.name}</h2>
             <p> R$ {product.price}</p>
             <button onClick={() => addToCart(product)}>Adicionar ao carrinho</button>
