921893b685fd6e7aef851cf6bbfdea8922d02ba6
okanharenan <okanharenan@gmail.com>
2026-08-12 21:07:54 -0300

    feat: add initial HTML and JavaScript files for API consumption; implement data fetching and rendering logic

---
+++ aula27/atividade3.js
@@ -0,0 +1,26 @@
+async function consumindoApi() {
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let data = await response.json();
+    console.log(data);
+    data.slice(0, 10).forEach((post) => {
+
+      const div = document.createElement("div");
+
+      const h1 = document.createElement("h1");
+      h1.textContent = post.title;
+
+      const p = document.createElement("p");
+      p.textContent = post.body;
+
+      div.appendChild(h1);
+      div.appendChild(p);
+      
+      document.body.appendChild(div);
+    });
+  } catch (error) {
+    console.error("Erro ao consumir a API:", error);
+  }
+}
+
+consumindoApi();
